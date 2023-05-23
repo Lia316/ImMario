dac5b520f0789ab71b74a56bb849239c771c0fcc
7bh7l0 <406ksy@naver.com>
2023-05-23 22:23:13 +0900

    feat: canvas on/off with left click
    revise some hierarchies

---
+++ ImMario/Assets/Scripts/SetActive.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActive : MonoBehaviour
+{
+    private bool state;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ARAVRInput.GetDown(ARAVRInput.Button.One))
+        {
+            if (state == true)
+            {
+                target.SetActive(false);
+                state = false;
+            }
+            else
+            {
+                target.SetActive(true);
+                state = true;
+            }
+        }
+
+    }
+}
