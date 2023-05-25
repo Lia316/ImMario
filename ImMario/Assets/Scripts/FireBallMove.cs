0179e1e743450da807104c7c16a5dd4105505d75
Lia316 <kjk0825@postech.ac.kr>
2023-05-26 07:34:19 +0900

    feat: fire ball bouncing logic

---
+++ ImMario/Assets/Scripts/FireBallMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallMove : MonoBehaviour
+{
+    public float thrust = 2000.0f;
+    private Rigidbody rb;
+    public float gravityScale = 1.0f; 
+    public static float globalGravity = -9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.left * 2000.0f);
+
+        Destroy(this.gameObject, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        rb.AddForce(gravity, ForceMode.Acceleration);
+        rb.AddForce(Vector3.left * thrust);
+
+        // if ball bounce off backward, destroy it
+        if (rb.velocity.x > 0.0f) {
+            Destroy(gameObject);
+        }
+    }
+}
