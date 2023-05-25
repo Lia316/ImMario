df937dc62071c92d086dc8cb39ac4da78a60827f
Lia316 <kjk0825@postech.ac.kr>
2023-05-26 04:50:38 +0900

    feat: player hand pop up when flower gotten

---
+++ ImMario/Assets/Scripts/FlowerGet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerGet : MonoBehaviour
+{
+    private GameObject playerHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHand = GameObject.Find("MarioRightHand");
+        playerHand.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Flower"))
+        {
+            print("collision with flower!");
+            Destroy(collision.gameObject);
+            playerHand.SetActive(true);
+        }
+    }
+}
