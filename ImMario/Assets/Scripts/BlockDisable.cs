1349386ac98381262b1a24fd3121af279350e69b
Lia316 <kjk0825@postech.ac.kr>
2023-05-26 04:01:16 +0900

    feat: disable question block when collide with head

---
+++ ImMario/Assets/Scripts/BlockDisable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDisable : MonoBehaviour
+{
+    public Material[] material;
+    private Renderer rend;
+    private bool isBoxEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.materials[0] = material[1];
+        rend.materials[1] = material[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(isBoxEnabled && collision.collider.CompareTag("Head"))
+        {
+            print("collision with box & head!");
+            var mats = rend.materials;
+            mats[1] = material[3];
+            mats[0] = material[2];
+            rend.materials = mats;
+            isBoxEnabled = false;
+        }
+    }
+}
