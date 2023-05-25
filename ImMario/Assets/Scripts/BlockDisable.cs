2379d91cffce8f788c0aeaa9990b202f14ec42fd
Lia316 <kjk0825@postech.ac.kr>
2023-05-26 05:53:53 +0900

    fix: move Gumba & place more blocks in the world

--- ImMario/Assets/Scripts/BlockDisable.cs
+++ ImMario/Assets/Scripts/BlockDisable.cs
@@ -5,31 +5,20 @@ using UnityEngine;
 public class BlockDisable : MonoBehaviour
 {
     public Material[] material;
-    public float moveHeight = 0.75f;
-    public float animationSpeed = 0.8f;
-
     private Renderer rend;
-    private Transform item;
-    private bool isBoxEnabled = true;
-    private float newHeight = 0.0f;
+    bool isBoxEnabled = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
         rend.enabled = true;
-
-        item = GameObject.Find("fire_flower").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isBoxEnabled && (newHeight <= moveHeight))
-        {
-            newHeight += Time.deltaTime * animationSpeed;
-            item.position = new Vector3(item.position.x, item.position.y + newHeight, item.position.z);
-        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,9 +26,16 @@ public class BlockDisable : MonoBehaviour
         if(isBoxEnabled && collision.collider.CompareTag("Head"))
         {
             print("collision with box & head!");
+
             var mats = rend.materials;
+
+            if (mats.Length == 2) {
             mats[1] = material[3];
             mats[0] = material[2];
+            }
+            else if (mats.Length == 1) {
+                mats[0] = material[2];
+            }
             rend.materials = mats;
 
             isBoxEnabled = false;
