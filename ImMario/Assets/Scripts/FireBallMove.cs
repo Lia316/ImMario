ad25e0dbfdfa7721cf0c96a6ea47ebf2fed9456a
Lia316 <kjk0825@postech.ac.kr>
2023-06-13 12:38:24 +0900

    feat: kill enemy with fireball

--- ImMario/Assets/Scripts/FireBallMove.cs
+++ ImMario/Assets/Scripts/FireBallMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FireBallMove : MonoBehaviour
 {
+    public GamePlayManager GamePlayEvent;
     public float thrust = 2000.0f;
     public float gravityScale = 1.0f; 
     public static float globalGravity = -9.81f;
@@ -15,6 +16,7 @@ public class FireBallMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GamePlayEvent = GameObject.Find("EventSystem").GetComponent<GamePlayManager>();
         rb = GetComponent<Rigidbody>();
         trans = GetComponent<Transform>();
 
@@ -32,5 +34,15 @@ public class FireBallMove : MonoBehaviour
         Vector3 gravity = globalGravity * gravityScale * Vector3.up;
         rb.AddForce(gravity, ForceMode.Acceleration);
         rb.AddForce(trans.forward * thrust);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Enemy"))
+        {
+            AudioManager.Instance.PlaySFX("stomp");
+            Destroy(collision.gameObject);
+            GamePlayEvent.coin_add();
+        }
     }
 }
